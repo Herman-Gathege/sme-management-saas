654f87b84b044294d578e005619ce910071b16b5
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2026-01-13 19:03:19 +0300

    sales working 5.0

--- frontend/src/features/sales/CreateSale.jsx
+++ frontend/src/features/sales/CreateSale.jsx
@@ -117,7 +117,7 @@ export default function CreateSale() {
 
     try {
       const token = localStorage.getItem("token");
-      const res = await fetch("http://127.0.0.1:5000/sales", {
+      const res = await fetch("http://127.0.0.1:5000/api/sales", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
