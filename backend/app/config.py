a36d3d1abb3ff125c7528ac64f1b086bf56b3fbd
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2026-02-11 15:32:01 +0300

    build push

--- backend/app/config.py
+++ backend/app/config.py
@@ -20,10 +20,10 @@ class Config:
     JWT_TOKEN_LOCATION = ["cookies"]
 
     # JWT_COOKIE_SECURE = True      # HTTPS only (Render uses HTTPS) change to True in production
-    JWT_COOKIE_SECURE=False #for local development
+    JWT_COOKIE_SECURE=True # False for local development
     JWT_COOKIE_HTTPONLY = True    # JS cannot read cookies
     JWT_COOKIE_SAMESITE = "Lax"
-    JWT_COOKIE_CSRF_PROTECT = False  # disable CSRF for development, enable in production
+    JWT_COOKIE_CSRF_PROTECT = True  # disable CSRF for development, enable in production
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
 
