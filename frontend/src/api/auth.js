5fe978a3bba5809a767e62bf5a387df171464957
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2026-02-11 17:18:57 +0300

    starting again from failed refresh fix

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 // /home/annewaithaka/personalprojects/sme-management-saas/frontend/src/api/auth.js
 
-const API = "https://smart-duka-backend.onrender.com/auth";
+const API = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const loginUser = async (data) => {
   const res = await fetch(`${API}/login`, {
